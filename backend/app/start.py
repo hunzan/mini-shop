dc3974008fd5e1556161ef1a0383b9f4afa515e0
林阿猴 <hunzan@gmail.com>
2026-01-27 12:55:58 +0800

    try to solve health problem

---
+++ backend/app/start.py
@@ -0,0 +1,10 @@
+# backend/app/start.py
+import os
+import uvicorn
+
+def main():
+    port = int(os.getenv("PORT", "8000"))
+    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
+
+if __name__ == "__main__":
+    main()
